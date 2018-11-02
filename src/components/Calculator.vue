eb9317624fcaa66327a82bac6ad3d33c0ef1705a
Kingsley Udenewu <kingzpacking@gmail.com>
2018-11-02 14:30:17 +0100

    Adding sign, percent and clear method

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="calculator">
-    <div class="display">{{ current }}</div>
-    <div class="btn ">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn ">%</div>
+    <div class="display">{{ current || '0' }}</div>
+    <div @click="clear" class="btn ">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn ">%</div>
     <div class="btn operator">/</div>
     <div class="btn ">7</div>
     <div class="btn ">8</div>
@@ -25,42 +25,54 @@
 
 <script>
 export default {
-
-  data: () =>{
-    return{
-      current: ''
+  data: () => {
+    return {
+      current: "1234"
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+    },
+    sign() {
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current / 100)}`;
     }
-
   }
-
-}
+};
 </script>
 
 <style scoped>
-  .calculator{
-    margin: 10px auto;
-    max-width: 300px;
-    font-size: 28px;
-    grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: minmax(50px, auto);
-    display: grid;
-  }
-  .display{
-    grid-column: 1 / 5;
-    background-color: #333333;
-    color: white;
-    font-weight: bold;
-    text-align: center;
-  }
-  .zero{
-    grid-column: 1 / 3;
-  }
-  .btn{
-    background-color: #eeeeee;
-    border: 1px solid #999999;
-  }
-  .operator{
-    background-color: orange;
-    color: white;
-  }
+.calculator {
+  margin: 10px auto;
+  max-width: 300px;
+  font-size: 28px;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+  display: grid;
+}
+.display {
+  grid-column: 1 / 5;
+  background-color: #333333;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  padding: 8px;
+}
+.zero {
+  grid-column: 1 / 3;
+}
+.btn {
+  background-color: #eeeeee;
+  border: 1px solid #999999;
+}
+.operator {
+  background-color: orange;
+  color: white;
+}
 </style>
